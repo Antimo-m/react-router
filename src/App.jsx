4f8df0659f5f063532939663f07ffb521b654654
Antimo-M <montellaantimo@live.it>
2025-12-09 14:56:53 +0100

    Struttura fatta, iniziato dal installare il router-dom, poi con postman verificato la chiamata ed ora creato le diverse pages e componenti con la struttura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Prodotti from "./pages/Prodotti"
+import Info from "./pages/ChiSiamo"
+import DefaultLayout from './assets/Layout/DefaultLayout'
 
 function App() {
- 
+
   return (
     <>
-     <BrowserRouter>
-     <Route path='/'/>
-     <Route path='/Chisiamo'/>
-     <Route path='/Prodotti'/>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout/>}>
+            <Route path='/' element={<Home />} />
+            <Route path='/Chisiamo' element={<Info />} />
+            <Route path='/Prodotti' element={<Prodotti />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
