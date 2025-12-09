e87cf84ba2432be77f44437aa421dc2ee369e780
Antimo-M <montellaantimo@live.it>
2025-12-09 16:50:41 +0100

    fatta anche la pages Chi siamo

--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -1,6 +1,18 @@
-function Info(){
-    return(
+function Info() {
+    return (
         <>
+            <div className="container text-center py-3">
+                <h3 className="text-warning">Chi siamo davvero?</h3>
+                <p className="text-info py-4">
+                    Chi Siamo
+                    Benvenuti nel nostro store! Siamo un team appassionato di qualità, design e innovazione, con l’obiettivo di offrire prodotti che semplificano la vita e portano un sorriso a chi li sceglie.
+
+                    La nostra missione è combinare convenienza, affidabilità e attenzione al cliente: selezioniamo attentamente ogni articolo, collaboriamo con fornitori fidati e garantiamo un servizio rapido e sicuro.
+
+                    Crediamo che ogni acquisto sia un’esperienza, non solo un prodotto. Per questo ci impegniamo a rendere la navigazione semplice, lo shopping piacevole e il supporto sempre disponibile.
+
+                    Scopri di più su di noi e lasciati ispirare dalle nostre collezioni. La tua soddisfazione è il nostro successo!</p>
+            </div>
         </>
     )
 }
